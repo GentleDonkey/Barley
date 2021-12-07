cc9f6a01ea85cbc165bf6f00b5c3da12d7006e8e
Zhiyan Qu <>
2021-12-06 22:48:43 -0500

    add import files to main.go init go mod

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "19900718qzyQZY"
+	dbName := "barley"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+func firstLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+	db := dbConn()
+	defer db.Close()
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", firstLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
+
+}
